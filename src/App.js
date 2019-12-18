42eec4fe2c0e914fee4824f11c6b904cea2441b7
NASA12345 <34130571+NASA12345@users.noreply.github.com>
2019-12-18 16:12:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+       GCI-2019-REACT-APP-<Nayan>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Google Code-in 2019
         </p>
         <a
           className="App-link"
